b0ffb833056fa06817b417e80b5b56fc1cae49b9
shashi <shashanksangaiit@gmail.com>
2026-09-05 20:49:31 +0530

    binary searh

---
+++ 5-9-26/BinarySearch.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class BinarySearch{
+    public static void main(String args[]){
+        Scanner s = new Scanner (System.in);
+        int n = s.nextInt();
+        int a[] = new int [n];
+        for(int i=0;i<n;i++){
+            a[i] = s.nextInt();
+        }
+        int k = s.nextInt();
+        int res = bs(a,k);
+        if(res!= -1){
+            System.out.println("element found at : " + res);
+        }
+        else{
+            System.out.println("element not found");
+        }
+        
+    }
+    public static int bs(int a[],int k){
+        int l = 0,u = a.length-1;
+        while(l<=u){ // ee equal to marchipothey test cases rale 
+            int mid = l+u/2;
+            if(a[mid] == k){
+                return mid +1; // to balance zero based index
+            }
+            else if(a[mid]>k){ // value takkava unndi kabbati lower bound loki povali 
+                u = mid -1;
+            }
+            else {
+                l = mid +1;
+            }
+        }
+        return -1;
+    }
+}
